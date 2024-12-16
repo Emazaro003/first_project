c04cfac7bf0a3546bea24576d726a5862b484eb0
Emanuel Ribeiro <emanuellalazaro@gmail.com>
2024-12-16 19:55:22 -0300

    fatorial com i32

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,17 @@ fn convert_to_int(data_input: &String) -> i32{
 }
 
 fn main() {
-    let mut numero_medias: String = String::new();
-    io::stdin().read_line(&mut numero_medias).expect("Erro ao ler entrada_fatorial");
+    
+    let mut entrada: String = String::new();
+    io::stdin().read_line(&mut entrada).expect("Erro ao ler entrada");
 
-    let mut numero_medias_int: i32 = convert_to_int(&numero_medias);
+    let mut fatorial: i32 = 1;
+    let entrada_int: i32 = convert_to_int(&entrada);
 
-
-    let mut qtd_alunos_reprovados: i32 = 0;
-
-    while numero_medias_int > 0{
-        let mut entrada_nota: String = String::new();
-        io::stdin().read_line(&mut entrada_nota).expect("Erro ao ler entrada_fatorial");
-
-        if convert_to_int(&entrada_nota) < 6 {qtd_alunos_reprovados = qtd_alunos_reprovados +1}
-
-
-        numero_medias_int = numero_medias_int -1;
+    for i in 1..entrada_int+1{
+        println!("i: {}", i);
+        fatorial = fatorial * i;
     }
 
-    println!("Reprovados: {}", qtd_alunos_reprovados)    
+    println!("fatorial: {}", fatorial)
 }
